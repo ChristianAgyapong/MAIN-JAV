a8a1ccf1489b010fddb4655ae9e67ed18e0cc790
Christian Agyapong <Christianagyapong2023@gmail.com>
2025-01-04 14:16:53 +0000

    I have addede a new if statement to this code

--- src/me.java
+++ src/me.java
@@ -1,8 +1,17 @@
 public class me {
     public static void main(String[] args) {
         // Your code here
-        System.out.println("ello, World!");
+        System.out.println("Hello, World!");
         System.out.println("I am a Java program."); 
         System.out.println("I am a student at Codecademy.");
+        
+        int number = 10;
+        if (number > 20) {
+            System.out.println("The number is greater than 5.");
+
+        }
+        else {
+            System.out.println("The number is not greater than 5.");
+        }
     }
 }
