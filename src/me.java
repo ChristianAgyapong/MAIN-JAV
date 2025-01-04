79400b2a2c3f3d8dad9d907f2b8fd0568ad8a344
Christian Agyapong <Christianagyapong2023@gmail.com>
2025-01-04 13:59:19 +0000

    Initial commit

---
+++ src/me.java
@@ -0,0 +1,7 @@
+public class me {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("ello, World!");
+        System.out.println("I am a Java program."); 
+    }
+}
